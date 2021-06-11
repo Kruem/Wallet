5f7b56dddea3aa28c9d8a0123d1d4cbfe35b004d
Kruem <lkgrayfox@gmail.com>
2021-06-10 22:26:03 -0500

    Base Models

---
+++ Wallet.Common/Entities/Ctity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wallet.Common.Entities
+{
+    public class Ctity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The filed {0} must contain less than {1} characteres.")]
+        [Required]
+        [Display(Name = "Document Type")]
+        public string Name { get; set; }
+    }
+}
---
+++ Wallet.Common/Entities/DocumentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Wallet.Common.Entities
+{
+    public class DocumentType
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The filed {0} must contain less than {1} characteres.")]
+        [Required]
+        [Display(Name = "Document Type")]
+        public string Name { get; set; }
+    }
+}
